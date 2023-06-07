1cfb83c4d0a5f3ae9790c6251d61d8dc7603275f
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-06-07 11:40:53 +0330

    add cors package

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const db = require("./db/mongoose");
@@ -7,6 +8,7 @@ const userRouter = require("./routes/user");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRouter);
