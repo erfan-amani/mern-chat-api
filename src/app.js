6dff03151fdd6c3b107756b2cbc0b76ab87e0e63
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-06-08 15:18:34 +0330

    socket connection and authoriztion

--- src/app.js
+++ src/app.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const socketio = require("socket.io");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const User = require("./models/user");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, { cors: { origin: "*" } });
+
+io.use(async (socket, next) => {
+  const token = socket.handshake.auth.token;
+
+  try {
+    if (!token) throw new Error();
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ _id: decoded.id });
+
+    if (!user) throw new Error();
+
+    socket.user = user;
+    socket.token = token;
+
+    next();
+  } catch (err) {
+    next(new Error("Not authorized. Please provide a valid token!"));
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.user);
+
+  socket.emit("test");
+});
 
 const db = require("./db/mongoose");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
-
-const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -18,4 +50,4 @@ app.use((error, req, res, next) => {
   res.status(500).send(error.message || error);
 });
 
-module.exports = app;
+module.exports = server;
