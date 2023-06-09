546e24e170ea3624f3dcf1ba209bbcf6497f93c8
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-06-09 13:55:32 +0330

    add users relation in room model

--- src/controllers/room.js
+++ src/controllers/room.js
@@ -40,25 +40,27 @@ const getRoom = async (req, res) => {
   try {
     const { sender, reciever } = req.query;
 
-    console.log({ sender, reciever });
+    let room = null;
+    let status = 200;
 
     // check room sender first
-    const roomA = await Room.findById(`${sender}:${reciever}`);
-    if (!!roomA) {
-      res.send(roomA);
-      return;
-    }
+    room = await Room.findById(`${sender}:${reciever}`);
 
     // check room reciver first
-    const roomB = await Room.findById(`${reciever}:${sender}`);
-    if (!!roomB) {
-      res.send(roomB);
-      return;
+    if (!room) {
+      room = await Room.findById(`${reciever}:${sender}`);
     }
 
     // create room
-    const room = new Room({ _id: `${sender}:${reciever}` }).save();
-    res.status(201).send(room);
+    if (!room) {
+      status = 201;
+      room = new Room({ _id: `${sender}:${reciever}` });
+      room.users.push(sender, reciever);
+      await room.save();
+    }
+
+    await room.populate("users");
+    res.status(status).send(room);
   } catch (err) {
     res.status(500).send(err.message || "Something went wrong!");
   }
--- src/models/room.js
+++ src/models/room.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const roomSchema = new mongoose.Schema(
   {
     _id: { type: String, required: true },
+    users: [{ type: mongoose.Types.ObjectId, ref: "User", required: true }],
   },
   { timestamps: true }
 );
