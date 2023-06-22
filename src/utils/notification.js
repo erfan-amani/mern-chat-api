02c250da0142e71eda7d34b04009387ceb179a0f
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-06-22 18:25:28 +0330

    remove logs

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,10 +1,7 @@
 const { getUserSocketConnection } = require("./socket");
 
 const sendNotification = (io, userId, notification) => {
-  console.log(userId);
   const socket = getUserSocketConnection(io, userId);
-
-  console.log(socket.to);
 
   socket.emit("notification", notification);
 };
